f194a0256039a7021b3c08d999fcd1d7c09de1df
Grace <lunay0325@gmail.com>
2019-03-04 02:38:56 +0800

    Implement the method in InverseRecude class and pass the test associated with it

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -9,13 +8,20 @@ public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        int randomNum = this.random.nextInt(3);
+        while ((number - randomNum) > 0) {
+            result.add(number - randomNum);
+            number -= randomNum;
+        }
+        return result;
     }
 }
