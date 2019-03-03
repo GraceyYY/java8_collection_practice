4ae268d56252a05df3c143fcfb83ddcd210020cf
Grace <lunay0325@gmail.com>
2019-03-04 02:18:30 +0800

    Implement all methods in Flaten class and pass all tests associated with it

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,33 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (Integer[] arr : this.array) {
+            result.addAll(Arrays.asList(arr));
+        }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> flaten = new ArrayList<>();
+        for (Integer[] arr : this.array) {
+            flaten.addAll(Arrays.asList(arr));
+        }
+        LinkedHashSet<Integer> tempSet = new LinkedHashSet<>(flaten);
+        List<Integer> result = new ArrayList<>(tempSet);
+        return result;
     }
 }
